c2a5ace6109985984e720d16c337f963449e69b7
khoacse <khoa12145@gmail.com>
2025-05-05 21:56:16 +0700

    Revert "BOOKINGUI"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,24 @@
-import React from "react";
-import BookingUI from "./BookingUI.jsx";
+import React from "react"; 
+import Home from "./pages/Home";
+import "./App.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Layout from "./layouts/MainLayouts";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/login";
 
-export default function App() {
-  return <BookingUI />;
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;
